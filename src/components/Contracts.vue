b16e2df37531cbc2ea76358034867d7182225251
abubakar9212 <abubakar.siddique@9212@miranz.net>
2018-12-27 15:06:25 +0500

    contracts

--- src/components/Contracts.vue
+++ src/components/Contracts.vue
@@ -36,27 +36,60 @@
                                         <br>
                                         <form action="#" data-parsley-validate="" novalidate="novalidate" @click.prevent=''>
                                             <div class="form-group">
-                                                <label for="userName">Amount of DinarCoin to mint</label>
-                                                <input type="number" placeholder="1DNC" class="form-control">
-                                            </div>
+                                                <label>Recipient Address</label>
+                                                <input type="text" placeholder="Address" required="required" data-parsley-id="8" class="form-control">
+                                           </div>
                                             <div class="form-group">
-                                                <label>Your Security Password*</label>
-                                                <input type="password" placeholder="Security Password" required="required" data-parsley-id="8" class="form-control">
-                                            <small>Please Confirm the send with your security password</small>
-                                        </div>
+                                                <label>Value</label>
+                                                <input type="text" placeholder="Amount" required="required" data-parsley-id="8" class="form-control">
+                                            </div>
                                             <div class="form-group text-right m-b-0">
-                                                <button data-toggle="modal" data-target="#sendToken-modal"  class="btn btn-default waves-effect waves-light">
-                                    Send
-                                </button>
-                                                <button type="reset" class="btn btn-default waves-effect waves-light m-l-5">
-                                    Cancel
+                                                <button @click="step = 2" data-target="#sendToken-modal"  class="btn btn-default waves-effect waves-light">
+                                    Generate Transaction
                                 </button>
                                             </div>
                                         </form>
                                     </div>
+
+                                    <div v-if="step === 2">
+                                        <div class=" p-0" style="margin-top: 15px">
+                                            <ul class="nav nav-tabs navtab-custom nav-justified">
+                                                <li class="active">
+                                                    <a href="#privateKey" data-toggle="tab" aria-expanded="true">
+                                                        <span>Private key</span>
+                                                    </a>
+                                                </li>
+                                                <li class="">
+                                                    <a href="#jasonFile" data-toggle="tab" aria-expanded="false">
+                                                        <span>Jason File</span>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                            <div class="tab-content">
+                                                <div id="privateKey" class="tab-pane active">
+                                                    <form data-v-92875dbe="" action="#" data-parsley-validate="" novalidate="novalidate">
+                                                        <div data-v-92875dbe="" class="form-group">
+                                                            <label data-v-92875dbe="">Enter Your Private Key</label>
+                                                            <input data-v-92875dbe="" type="password" placeholder="" required="required" data-parsley-id="8" class="form-control">
+                                                        </div>
+
+                                                        <div data-v-92875dbe="" class="form-group text-right m-b-0">
+
+                                                        <button data-v-92875dbe="" data-toggle="modal" data-target="#sendToken-modal" class="btn btn-default waves-effect waves-light">
+                                                        Send Transaction
+                                                    </button>
+                                                    </div>
+                                                    </form>
+                                                </div>
+                                                <div id="jasonFile" class="tab-pane ">
+                                                    <span>Jason File</span>
+                                                </div>
+                                            </div>
+                                        </div><!-- /.modal-content -->
+
+                                    </div>
                                 </div>
                             </div>
-
                         <div id="menu1" class="tab-pane fade">
                             <h3 style="color: #ffffff">Menu 2</h3>
                             <p>Some content in menu 2.</p>
@@ -71,51 +104,7 @@
                         </div>
                     </div>
                 </div>
-                <!--<div class="row">-->
-                    <!--<div class="col-md-4 col-md-offset-2">-->
-                        <!--<div class="form-group">-->
-                            <!--<label>Contract Address</label>-->
-                            <!--<input type="text" class="form-control" placeholder="notopia.eth or 0xD3E00087">-->
-                        <!--</div>-->
-                    <!--</div>-->
-                    <!--<div class="col-md-4">-->
-                        <!--<div class="form-group">-->
-                            <!--<label>Select Existing Contract</label>-->
-                            <!--<select class="form-control">-->
-                                <!--<option>Select a contract ...</option>-->
-                            <!--</select>-->
-                        <!--</div>-->
-                    <!--</div>-->
-                <!--</div>-->
-                <!--<div class="row">-->
-                    <!--<div class="col-md-8 col-md-offset-2">-->
-                        <!--<div class="form-group">-->
-                            <!--<label>Abi / JSON Interface</label>-->
-                            <!--<textarea class="form-control" placeholder='[{ "type":"contructor", "inputs": [{ "name":"param1", "type":"uint256", "indexed":true }], "name":"Event" }, { "type":"function", "inputs": [{"name":"a", "type":"uint256"}], "name":"foo", "outputs": [] }] '></textarea>-->
-                        <!--</div>-->
-                    <!--</div>-->
-                <!--</div>-->
-                <!--<div class="row">-->
-                    <!--<div class="col-md-8 col-md-offset-2">-->
-                        <!--<div class="form-group">-->
-                            <!--<button class="btn btn-lg btn-primary" @click="step = 2">Access</button>-->
-                        <!--</div>-->
-                    <!--</div>-->
-                <!--</div>-->
-                <!--<div class="row" v-if="step == 2">-->
-                    <!--<div class="col-md-8 col-md-offset-2">-->
-                        <!--<h3>Read / Write Contract</h3>-->
-                        <!--<div class="form-group">-->
-                            <!--<select class="form-control">-->
-                                <!--<option value="">Name</option>-->
-                            <!--</select>-->
-                        <!--</div>-->
-                        <!--<div class="form-group">-->
-                            <!--<input type="text" class="form-control" value="Adshares Token">-->
-                        <!--</div>-->
-                    <!--</div>-->
-                <!--</div>-->
-            </div>
+    </div>
 </template>
 <script>
     export default {
