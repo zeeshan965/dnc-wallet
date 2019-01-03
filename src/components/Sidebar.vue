e4c062908660a9b400b9eb5a228e84073756e118
abubakar9212 <abubakar.siddique@9212@miranz.net>
2019-01-03 21:50:53 +0500

    modal shown main page

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,78 +1,150 @@
 <template>
-    <div class="left side-menu" style="overflow: hidden;text-align:left">
-        <div class="sidebar-inner slimscrollleft" style="overflow: hidden;">
+    <!--<div class="left side-menu" style="overflow: hidden;text-align:left">-->
+        <!--<div class="sidebar-inner slimscrollleft" style="overflow: hidden;">-->
+            <!--<div id="sidebar-menu">-->
+                <!--<ul>-->
+                    <!--<li class="text-muted menu-title">Main</li>-->
+
+                    <!--<li>-->
+                        <!--<router-link to="/" class="waves-effect waves-primary"><img-->
+                                <!--src="assets/images/icon/User-icon.png">&nbsp;&nbsp;&nbsp;<span>-->
+                                <!--Your Wallet </span></router-link>-->
+                    <!--</li>-->
+                    <!--<li>-->
+                        <!--<a data-toggle="modal" data-target="#new_wallet" href="#"-->
+                           <!--class="waves-effect waves-primary"><img-->
+                                <!--src="assets/images/icon/math-add-icon.png">&nbsp;&nbsp;&nbsp;<span>-->
+                                <!--New Wallet </span></a>-->
+                    <!--</li>-->
+                    <!--<li>-->
+                        <!--<router-link to="/contracts" class="waves-effect waves-primary"><img-->
+                                <!--src="assets/images/icon/Play-Books-icon.png">&nbsp;&nbsp;&nbsp;<span>-->
+                               <!--DNC Contract </span></router-link>-->
+                    <!--</li>-->
+                    <!--<li>-->
+                        <!--<router-link to="/importaccounts" class="waves-effect waves-primary"><img-->
+                                <!--src="assets/images/icon/Alternative-Explorer-2-icon.png">&nbsp;&nbsp;&nbsp;<span>-->
+                                <!--Import Accounts </span></router-link>-->
+                    <!--</li>-->
+                    <!--<li>-->
+                        <!--<a target="_blank" href="http://54.169.155.78/" class="waves-effect waves-primary"><img-->
+                                <!--src="assets/images/icon/dinar-icon.png">&nbsp;&nbsp;&nbsp;<span>-->
+                                <!--DNC Explorer </span></a>-->
+                    <!--</li>-->
+                    <!--<li>-->
+                        <!--<a target="_blank" href="http://13.59.238.20/en" class="waves-effect waves-primary"><img-->
+                                <!--src="assets/images/icon/dnd-icon.png">&nbsp;&nbsp;&nbsp;<span>-->
+                                <!--Mint DNC </span></a>-->
+                    <!--</li>-->
+                    <!--<li>-->
+                        <!--<router-link to="/checkTxStatus" class="waves-effect waves-primary"><img-->
+                                <!--src="assets/images/icon/hashtag.png">&nbsp;&nbsp;&nbsp;<span>-->
+                                <!--Check Tx Status </span></router-link>-->
+                    <!--</li>-->
+                    <!--&lt;!&ndash;<li>&ndash;&gt;-->
+                    <!--&lt;!&ndash;<router-link to="/profile" >&ndash;&gt;-->
+                    <!--&lt;!&ndash;<a  class="waves-effect waves-primary"><img src="assets/images/icon/User-blue-icon.png">&nbsp;&nbsp;&nbsp;<span>&ndash;&gt;-->
+                    <!--&lt;!&ndash;Profile </span></a>&ndash;&gt;-->
+                    <!--&lt;!&ndash;</router-link>&ndash;&gt;-->
+                    <!--&lt;!&ndash;</li>&ndash;&gt;-->
+                    <!--&lt;!&ndash;<li class="has_sub">&ndash;&gt;-->
+                    <!--&lt;!&ndash;<a href="#" class="waves-effect waves-primary"><img src="assets/images/icon/settings-icon.png">&nbsp;&nbsp;&nbsp;&ndash;&gt;-->
+                    <!--&lt;!&ndash;<span> Setting </span>&ndash;&gt;-->
+                    <!--&lt;!&ndash;</a>&ndash;&gt;-->
+                    <!--&lt;!&ndash;<ul class="list-unstyled">&ndash;&gt;-->
+                    <!--&lt;!&ndash;<li><a href="">Increase Your Security</a></li>&ndash;&gt;-->
+                    <!--&lt;!&ndash;<li><a href="">Paper Backup (PDF)</a></li>&ndash;&gt;-->
+                    <!--&lt;!&ndash;<li><a href="">Key Import (Advanced)</a></li>&ndash;&gt;-->
+
+                    <!--&lt;!&ndash;</ul>&ndash;&gt;-->
+                    <!--&lt;!&ndash;</li>&ndash;&gt;-->
+                    <!--&lt;!&ndash;<li>&ndash;&gt;-->
+                    <!--&lt;!&ndash;<a href="blockchain_explore.html" class="waves-effect waves-primary"><img src="assets/images/icon/Alternative-Explorer-2-icon.png">&nbsp;&nbsp;&nbsp;<span>&ndash;&gt;-->
+                    <!--&lt;!&ndash;Blockchain Explorer </span></a>&ndash;&gt;-->
+                    <!--&lt;!&ndash;</li>&ndash;&gt;-->
+                <!--</ul>-->
+                <!--<div class="clearfix"></div>-->
+            <!--</div>-->
+            <!--<div class="clearfix"></div>-->
+        <!--</div>-->
+
+    <!--</div>-->
+    <div class="left side-menu">
+        <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 261px;"><div class="sidebar-inner slimscrollleft" style="overflow: hidden; width: auto; height: 261px;">
+
             <div id="sidebar-menu">
                 <ul>
                     <li class="text-muted menu-title">Main</li>
 
                     <li>
-                        <router-link to="/" class="waves-effect waves-primary"><img
-                                src="assets/images/icon/User-icon.png">&nbsp;&nbsp;&nbsp;<span>
-                                Your Wallet </span></router-link>
+                        <a href="index.html" class="waves-effect waves-primary subdrop"><img src="assets/images/icon/User-icon.png">&nbsp;&nbsp;&nbsp;<span> Your Wallet </span></a>
                     </li>
-                    <li>
-                        <a data-toggle="modal" data-target="#new_wallet" href="#"
-                           class="waves-effect waves-primary"><img
-                                src="assets/images/icon/math-add-icon.png">&nbsp;&nbsp;&nbsp;<span>
-                                New Wallet </span></a>
-                    </li>
-                    <li>
-                        <router-link to="/contracts" class="waves-effect waves-primary"><img
-                                src="assets/images/icon/Play-Books-icon.png">&nbsp;&nbsp;&nbsp;<span>
-                               DNC Contract </span></router-link>
-                    </li>
-                    <li>
-                        <router-link to="/importaccounts" class="waves-effect waves-primary"><img
-                                src="assets/images/icon/Alternative-Explorer-2-icon.png">&nbsp;&nbsp;&nbsp;<span>
-                                Import Accounts </span></router-link>
-                    </li>
-                    <li>
-                        <a target="_blank" href="http://54.169.155.78/" class="waves-effect waves-primary"><img
-                                src="assets/images/icon/dinar-icon.png">&nbsp;&nbsp;&nbsp;<span>
-                                DNC Explorer </span></a>
-                    </li>
-                    <li>
-                        <a target="_blank" href="http://13.59.238.20/en" class="waves-effect waves-primary"><img
-                                src="assets/images/icon/dnd-icon.png">&nbsp;&nbsp;&nbsp;<span>
-                                Mint DNC </span></a>
-                    </li>
-                    <li>
-                        <router-link to="/checkTxStatus" class="waves-effect waves-primary"><img
-                                src="assets/images/icon/hashtag.png">&nbsp;&nbsp;&nbsp;<span>
-                                Check Tx Status </span></router-link>
-                    </li>
-                    <!--<li>-->
-                    <!--<router-link to="/profile" >-->
-                    <!--<a  class="waves-effect waves-primary"><img src="assets/images/icon/User-blue-icon.png">&nbsp;&nbsp;&nbsp;<span>-->
-                    <!--Profile </span></a>-->
-                    <!--</router-link>-->
-                    <!--</li>-->
-                    <!--<li class="has_sub">-->
-                    <!--<a href="#" class="waves-effect waves-primary"><img src="assets/images/icon/settings-icon.png">&nbsp;&nbsp;&nbsp;-->
-                    <!--<span> Setting </span>-->
-                    <!--</a>-->
-                    <!--<ul class="list-unstyled">-->
-                    <!--<li><a href="">Increase Your Security</a></li>-->
-                    <!--<li><a href="">Paper Backup (PDF)</a></li>-->
-                    <!--<li><a href="">Key Import (Advanced)</a></li>-->
 
-                    <!--</ul>-->
-                    <!--</li>-->
-                    <!--<li>-->
-                    <!--<a href="blockchain_explore.html" class="waves-effect waves-primary"><img src="assets/images/icon/Alternative-Explorer-2-icon.png">&nbsp;&nbsp;&nbsp;<span>-->
-                    <!--Blockchain Explorer </span></a>-->
-                    <!--</li>-->
+                    <li>
+                        <a data-toggle="modal" data-target="#new_wallet" href="#" class="waves-effect waves-primary"><img src="assets/images/icon/math-add-icon.png">&nbsp;&nbsp;&nbsp;<span> New Wallet </span></a>
+                    </li>
+
+                    <li>
+                        <a href="buy_sell.html" class="waves-effect waves-primary"><img src="assets/images/icon/Bitcoin-icon.png">&nbsp;&nbsp;&nbsp;<span> Buy/Sell Assets </span></a>
+                    </li>
+
+                    <li>
+                        <a href="exchange.html" class="waves-effect waves-primary"><img src="assets/images/icon/Folder-Data-icon.png">&nbsp;&nbsp;&nbsp;<span> Exchange Assets </span></a>
+                    </li>
+
+                    <li>
+                        <a href="address_book.html" class="waves-effect waves-primary"><img src="assets/images/icon/Play-Books-icon.png">&nbsp;&nbsp;&nbsp;<span> Your Address Book </span></a>
+                    </li>
+
+                    <li>
+                        <a href="new_address.html" class="waves-effect waves-primary"><img src="assets/images/icon/math-add-icon.png">&nbsp;&nbsp;&nbsp;<span> New Address </span></a>
+                    </li>
+
+                    <li>
+                        <a href="profile.html" class="waves-effect waves-primary"><img src="assets/images/icon/User-blue-icon.png">&nbsp;&nbsp;&nbsp;<span> Profile </span></a>
+                    </li>
+
+
+
+                    <li class="has_sub">
+                        <a href="#" class="waves-effect waves-primary"><img src="assets/images/icon/settings-icon.png">&nbsp;&nbsp;&nbsp; <span> Setting </span>
+                        </a>
+                        <ul class="list-unstyled">
+                            <li><a href="security.html">Increase Your Security</a></li>
+                            <li><a href="pdf.html">Paper Backup (PDF)</a></li>
+                            <li><a href="key_import.html">Key Import (Advanced)</a></li>
+
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="mining_fees.html" class="waves-effect waves-primary"><img src="assets/images/icon/Security-icon.png">&nbsp;&nbsp;&nbsp;<span> Earn Mining Fees </span></a>
+                    </li>
+
+                    <li>
+                        <a href="blockchain_explore.html" class="waves-effect waves-primary"><img src="assets/images/icon/Alternative-Explorer-2-icon.png">&nbsp;&nbsp;&nbsp;<span> Blockchain Explorer </span></a>
+                    </li>
+
+
+
+
+
                 </ul>
                 <div class="clearfix"></div>
             </div>
+
+
             <div class="clearfix"></div>
-        </div>
+        </div><div class="slimScrollBar" style="background: rgb(75, 84, 94); width: 5px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 116.247px; visibility: visible;"></div><div class="slimScrollRail" style="width: 5px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
 
     </div>
 </template>
 <script>
     export default {
         mounted: function () {
+            $(window).on('load',function(){
+        $('#new_wallet').modal('show');
+    });
         }
     }
 </script>
