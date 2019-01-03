507af14dbceffadf55786b14ef76a8ccf1e85f66
Rizwan Khan <rizwan.ud.din@miranz.net>
2019-01-03 20:41:04 +0500

    Response issue

--- src/components/importaccounts.vue
+++ src/components/importaccounts.vue
@@ -181,9 +181,6 @@
     var WalletService = require('./../services/wallet');
     import router from './../router';
     import {addressesBlancess, dncAddressesBlancess} from '../services/wallet';
-
-    var getAccountFroomJson = require('./../services/getAccountFromJson');
-
     var DncTokenBalance = require('./../services/getTokenBalance');
 
     var Web3 = require('web3');
@@ -218,11 +215,7 @@
                 e.preventDefault();
                 //    console.log(e);
                 this.fileName = e.target.files[0].name;
-                console.log('File Name ' + this.fileName);
-
                 var newFileName = this.fileName.split('.');
-                console.log('New File name zzzzzzzzzzzzzzzz' + newFileName[1].length);
-
                 if (newFileName[1].length > 46 || newFileName[1].length < 46) {
                     alertify.set('notifier', 'position', 'top-right');
                     alertify.error('Invalid file format');
@@ -230,26 +223,6 @@
                     return false;
                 }
 
-                // if (this.fileName.includes('.pdf')) {
-                //
-                //      alertify.set('notifier','position', 'top-right');
-                //      alertify.error('Invalid file format');
-                //
-                //     setTimeout(() => {
-                //         window.location.href = "importaccounts"
-                //
-                //     }, 2000);
-                //
-                // }
-                // if (this.fileName.includes('.png')) {
-                //     setTimeout(() => {
-                //
-                //      alertify.set('notifier','position', 'top-right');
-                //      alertify.error('Invalid file format');
-                //     }, 2000);
-                //     window.location.href = "importaccounts"
-                //
-                // }
 
                 var _this = this;
                 // Reference to the DOM input element
@@ -279,7 +252,7 @@
                 var _this = this;
                 e.preventDefault();
                 var fromValue = _this.userKey;
-                console.log(fromValue);
+
                 _this.submitted = true;
 
                 _this.$validator.validate().then(valid => {
@@ -290,7 +263,6 @@
 
 
                             _this.walletResponse = WalletService.unlockAccount(_this.userKey, '123456789');
-                            console.log('Unclocked ===> ' + _this.walletResponse);
                             if (_this.walletResponse === false) {
                                 alertify.set('notifier', 'position', 'top-right');
                                 alertify.error('Invalid Private key');
@@ -306,29 +278,23 @@
                                     var myBalance;
                                     var balance = WalletService.getBalance(_this.walletResponse.address).then((res) => {
                                         myBalance = res;
-                                        console.log('Response inside get balance ' + myBalance);
                                     });
                                     setTimeout(() => {
-                                        console.log('Balance i get zzzzzzzzzzzzzz' + myBalance);
                                         WalletService.addressesBlancess.push(myBalance);
-                                        console.log('First address is ' + addressesBlancess[0]);
                                     }, 3000);
 
                                     //DNC Private Balance
                                     var myDNCBalance;
                                     var balancednc = DncTokenBalance.getDncBalance(_this.walletResponse.address).then((dncres) => {
                                         myDNCBalance = dncres;
-                                        console.log("Dnc issdsdsds balcne is for then resposne" + myDNCBalance);
                                     });
 
                                     setTimeout(() => {
                                         console.log("Balcne i get for dnc is" + myDNCBalance);
                                         WalletService.dncAddressesBlancess.push(myDNCBalance);
-                                        console.log("DNC first Address is " + dncAddressesBlancess[0]);
                                     }, 3000);
 
                                 }, 1000);
-                                // alert('Unlocked address ===> ' + WalletService.addresses);
 
                                 router.push('/');
 
@@ -372,28 +338,19 @@
 
                 var _this = this;
 
-                alert(_this.user.password);
-
-
-                console.log("Button  zzzzzzzzzzzzzzzzzz");
-
                 e.preventDefault();
 
                 _this.submitted = true;
 
                     var password = _this.user.password;
-
-                    console.log('Userzzzz password ' + password);
-
                     var jsonBackResposne=false;
                     var jsonResponseError =false;
 
 
-                    console.log("Fucnaaiton json data " + JSON.stringify(_this.json_Data));
                     setTimeout(function () {
 
                             _this.getAccountFromJson().then((res)=>{
-                                console.log("Response isss"  + res);
+
                                 jsonBackResposne = true;
                                 if(jsonBackResposne){
                                     _this.listing();
@@ -403,7 +360,6 @@
                                 jsonResponseError = true;
                                 console.log("Error  is" +e);
                                 if (jsonResponseError) {
-                    alert("Azeem");
                                     alertify.set('notifier', 'position', 'top-right');
                                     alertify.error("Possibly Wrong Password");
                                     _this.user.password = '';
@@ -416,72 +372,28 @@
 
 
                     },500);
-
-                          // jsonBackResposne = res;
-                    // }).catch((e) => {
-                    //     console.log("Error " + e);
-                    //     jsonResponseError = e;
-                    //
-                    //
-                    // });
-
-
-                    // console.log("Accoutn" + JSON.stringify(account.privateKey));
-
-                    // var resposne =await getAccountFroomJson.getprivateKeyFromJson(_this.json_Data, _this.user.password).then((res) => {
-                    //     jsonBackResposne = res;
-                    // }).catch((e) => {
-                    //     console.log("Error " + e);
-                    //     jsonResponseError = e;
-                    //
-                    //     _this.loader =false;
-                    // });
-                    //
-
-
-                // $('#spinnerr').hide();
-
-
-
-
-
-                //     }
-                // });
-
-
             },
             listing:async function () {
 
                 var _this =this;
                 WalletService.addresses.push(_this.json_Data.address);
 
-                console.log("Json resposne is  " + _this.json_Data);
-
-
                 //Eth key Store Balance
                 var keystoreBalance;
                 var balance = await WalletService.getBalance(_this.json_Data.address).then((res) => {
                     keystoreBalance = res;
-                    console.log('Response inside get balance ' + keystoreBalance);
                 });
 
-
                 WalletService.addressesBlancess.push(keystoreBalance);
-                console.log('First address is ' + addressesBlancess[0]);
-
 
                 //DNC  Keystore Balance
 
                 var myDNCkeyStoreBalance;
                 var keystoreBalancednc = await DncTokenBalance.getDncBalance(_this.json_Data.address).then((dncres) => {
                     myDNCkeyStoreBalance = dncres;
-                    console.log("Dnc issdsdsds balcne keystore is for then resposne" + myDNCkeyStoreBalance);
                 });
 
-
                 WalletService.dncAddressesBlancess.push(myDNCkeyStoreBalance);
-                console.log("DNC first keystotre Address is " + dncAddressesBlancess[0]);
-
 
                 _this.loader = false;
                 router.push('/');
